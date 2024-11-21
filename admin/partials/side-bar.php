cdb40caa4df8358a5b4cb0cf89759139d5322da8
robb19 <lenonrobbie18@gmail.com>
2024-11-22 05:21:49 +0800

    Logout function was successfully implementedgit add .

--- admin/partials/side-bar.php
+++ admin/partials/side-bar.php
@@ -1,7 +1,7 @@
 <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary vh-100">
     <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+            <h5 class="offcanvas-title" id="sidebarMenuLabel">Company Name</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
@@ -14,6 +14,7 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <i class="fa-solid fa-book fa-fw me-2"></i>
                         Subjects
                     </a>
                 </li>
@@ -24,17 +25,16 @@
                     </a>
                 </li>                
             </ul>
-           
+
             <hr class="my-3">
 
-            <ul class="nav flex-column mb-auto">
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <i class="fa-solid fa-right-to-bracket fa-fw me-2"></i>                        
-                        Logout
-                    </a>
-                </li>
-            </ul>
+            <!-- Logout Button -->
+            <div class="mt-auto">
+                <a href="logout.php" class="btn btn-danger w-100 d-flex align-items-center justify-content-center gap-2">
+                   
+                    Logout
+                </a>
+            </div>
         </div>
     </div>
 </div>
